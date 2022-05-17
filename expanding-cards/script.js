c2021e63a088c7d5bf0c95863b7e41c83ec059db
David Olivares <daveolivares01@gmail.com>
2022-05-16 21:00:30 -0700

    Adds expanding cards project

---
+++ expanding-cards/script.js
@@ -0,0 +1,16 @@
+// get all elements with class 'panel'
+const panels = document.querySelectorAll('.panel');
+
+// forEach is a high order func
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
